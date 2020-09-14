42461a2f7b9c546ec3f31f3d7de866473156b7e6
abdulkalam556 <34823802+abdulkalam556@users.noreply.github.com>
2020-09-14 12:27:45 +0530

    adding about page link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,6 +20,9 @@ const Navbar = ({ title, icon }) => {
     <Fragment>
       <li>Hello {user && user.name}</li>
       <li>
+        <Link to='/about'>Login</Link>
+      </li>
+      <li>
         <a onClick={onLogout} href='#!'>
           <i className='fas fa-sign-out-alt'></i>{' '}
           <span className='hide-sm'>Logout</span>
